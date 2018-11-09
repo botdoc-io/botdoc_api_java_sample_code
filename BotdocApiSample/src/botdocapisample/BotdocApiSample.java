4874f402ce4b042a97ef3c45f8b4472ffb929653
Rafael Juzwiak <rafael_juzwiak@hotmail.com>
2018-11-09 15:13:39 -0200

    init project

---
+++ BotdocApiSample/src/botdocapisample/BotdocApiSample.java
@@ -0,0 +1,12 @@
+package botdocapisample;
+
+/**
+ *
+ * @author Rafael Juzwiak
+ */
+
+public class BotdocApiSample {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
